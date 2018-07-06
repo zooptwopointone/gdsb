effebbac505d58e7efbddae4cd1b1de56cbb5053
Rewati Raman <rewati.raman@ytel.com>
2018-07-06 14:12:09 -0700

    Removed Panic log and adding Printf instead

--- config.go
+++ config.go
@@ -30,7 +30,7 @@ type Configuration struct {
 }
 
 //LoadConfigurationsFromDir Load Configurations From Dir
-func loadConfigurationsFromDir(resourcesDir string, configuration Configuration) error {
+func LoadConfigurationsFromDir(resourcesDir string, configuration Configuration) error {
 	v := viper.New()
 	v.SetConfigName("appConfig")
 	v.SetConfigType("yaml")
@@ -47,8 +47,8 @@ func loadConfigurationsFromDir(resourcesDir string, configuration Configuration)
 //LoadConfigurations loads configurations
 func LoadConfigurations(c Configuration) error {
 	resourceDir, _ := getResourceDir(c.EnvName)
-	if err := loadConfigurationsFromDir(resourceDir, c); err != nil {
-		log.Panicf("Error while loading Configuration. Error: %v", err)
+	if err := LoadConfigurationsFromDir(resourceDir, c); err != nil {
+		log.Printf("Error while loading Configuration. Error: %v\n", err)
 		return err
 	}
 	return nil
@@ -61,7 +61,7 @@ func getResourceDir(env string) (string, error) {
 			resourceDir = os.Getenv(env)
 		}
 		if len(resourceDir) == 0 {
-			log.Panic("Resource dir evironement is empty... Please set GDSB_RESOURCE env or place config file in current directory")
+			log.Printf("Resource dir evironement is empty... Please set GDSB_RESOURCE env or place config file in current directory\n")
 			return resourceDir, errors.New("Resource dir evironement is empty... Please set GDSB_RESOURCE env or place config file in current directory")
 		}
 		return resourceDir, nil
